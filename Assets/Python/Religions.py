7d24c2e9ea3048bad513cf627738577319a723c2
crossphazer <crossphazer@hotmail.com>
2025-02-16 12:56:43 -0500

    More attempts at respawning Jerusalem
    Prevent Judaism from being founded unless Jerusalem exists; as a failsafe, found Judaism around 500 BC if still no Jerusalem.
    Further increase La Coruna priority to Spain.

--- Assets/Python/Religions.py
+++ Assets/Python/Religions.py
@@ -62,7 +62,12 @@ def onBuildingBuilt(city, iBuilding):
 
 @handler("BeginGameTurn")
 def checkFoundReligions(iGameTurn):
-	if not game.isReligionFounded(iJudaism) and iGameTurn >= year(-1200) - turns(data.iSeed % 4):
+	# only found Judaism if Jerusalem exists; Jerusalem is re-founded several times during the game
+	if not game.isReligionFounded(iJudaism) and plots.surrounding(tJerusalem, radius=1).cities().count() > 0 and iGameTurn >= year(-1000) - turns(data.iSeed % 6):
+		foundReligion(selectHolyCity(plots.regions(rEgypt, rLevant, rMesopotamia), tJerusalem, False), iJudaism)
+
+	# if Jerusalem still doesn't exist, found it randomly around the year 500 BC
+	if not game.isReligionFounded(iJudaism) and iGameTurn >= year(-500) - turns(data.iSeed % 4):
 		foundReligion(selectHolyCity(plots.regions(rEgypt, rLevant, rMesopotamia), tJerusalem, False), iJudaism)
 
 	if not game.isReligionFounded(iHinduism) and iGameTurn >= year(-1000) - turns(data.iSeed % 4):
